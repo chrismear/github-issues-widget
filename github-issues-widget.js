bf252773a739d65ccaaff4610b0a34a5f0eb0444
Chris Mear <chris@feedmechocolate.com>
2010-03-22 16:36:10 +0000

    Make issue titles link to the issue on GitHub.

--- github-issues-widget.js
+++ github-issues-widget.js
@@ -4,8 +4,11 @@ GithubIssuesWidget.go = function () {
   $.getJSON(this.url, function (data) {
     var list = $('<ul></ul>');
     $.each(data.issues, function (issueIndex, issue) {
+      var issueUrl = 'http://github.com/' + GITHUB_ISSUES_USER + '/' + GITHUB_ISSUES_REPO + '/issues#issue/' + issue.number;
       var issueHtml = "<li>";
+      issueHtml += '<a href="' + issueUrl + '">';
       issueHtml += issue.title;
+      issueHtml += "</a>";
       $.each(issue.labels, function (labelIndex, label) {
         issueHtml += '<span class="label">' + label + '</span>';
       });
